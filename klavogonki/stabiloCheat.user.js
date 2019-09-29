216c5b4c70e9649e00e7dd04c7d930aef905439a
gaben-s-agent <33730925+gaben-s-agent@users.noreply.github.com>
2019-09-29 22:25:34 +0500

    Create stabiloCheat.user.js

---
+++ klavogonki/stabiloCheat.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name           stabiloCheat
+// @version        0.01
+// @namespace      klavogonki
+// @author         490344
+// @include        http://klavogonki.ru/g/*
+// @grant          unsafeWindow
+// ==/UserScript==
+
+(function() {
+	var proxied = window.XMLHttpRequest.prototype.send;
+	var length;
+    window.XMLHttpRequest.prototype.send = function () {
+        this.addEventListener('load', function () {
+            try {
+                var json = JSON.parse(this.responseText);
+                if ('text' in json) {
+                    length = (json.text.length);
+                }
+            } catch (e) {}
+        }.bind(this));
+        return proxied.apply(this, [].slice.call(arguments));
+    };
+
+	unsafeWindow.timeForSpeed = function(speed) {
+		var time = (60 / (speed / length));
+		if (time > 60) {
+			time = (Math.floor(time / 60) + ':' + time % 60)
+		}
+		return console.log('' + time);
+	};
+
+})();
