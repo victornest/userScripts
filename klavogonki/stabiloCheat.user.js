e723f78a99ea91a652730f35612056e52da3a92f
gaben-s-agent <33730925+gaben-s-agent@users.noreply.github.com>
2019-10-13 19:38:12 +0500

    Update stabiloCheat.user.js

--- klavogonki/stabiloCheat.user.js
+++ klavogonki/stabiloCheat.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           stabiloCheat
-// @version        0.03
+// @version        0.04
 // @namespace      klavogonki
 // @author         490344
 // @include        http://klavogonki.ru/g/*
@@ -29,5 +29,35 @@
 		}
 		return console.log(time);
 	};
+
+	var injPlace = document.getElementById('params').getElementsByClassName('rc')[0];
+	var injNode = document.createElement('div');
+	var inputSpeed = document.createElement('input');
+	var tfs = document.createElement('div');
+
+	injNode.insert(inputSpeed);
+	injNode.insert(tfs);
+	injPlace.insertBefore(injNode, injPlace.childNodes[3]);
+
+	inputSpeed.style.setProperty('display', 'inline');
+	inputSpeed.style.setProperty('max-width', '40px');
+	inputSpeed.style.setProperty('border', 'solid 1px #d5d5d5');
+	inputSpeed.style.setProperty('text-align', 'center');
+	inputSpeed.addEventListener('keyup', function () {
+		document.getElementById('tfs').innerText = getTime(this.value);
+	});
+
+	tfs.style.setProperty('display', 'inline');
+	tfs.style.setProperty('padding-left', '5px');
+	tfs.setAttribute('id', 'tfs');
+
+	function getTime(speed) {
+		var time = (60 / (speed / length));
+		if (time > 60) {
+			time = (Math.floor(time / 60) + ':' + time % 60)
+		}
+		return time;
+	}
+
 
 })();
