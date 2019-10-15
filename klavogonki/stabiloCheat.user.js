0fc6cdcbb5e0ea51864dd73d725a619c278aae1d
gaben-s-agent <33730925+gaben-s-agent@users.noreply.github.com>
2019-10-16 02:12:56 +0500

    added progressBar

--- klavogonki/stabiloCheat.user.js
+++ klavogonki/stabiloCheat.user.js
@@ -1,30 +1,41 @@
 // ==UserScript==
 // @name           stabiloCheat
-// @version        0.05
+// @version        0.06
 // @namespace      klavogonki
 // @author         490344
 // @include        http://klavogonki.ru/g/*
 // @grant          unsafeWindow
 // ==/UserScript==
 
-(function() {
+
+document.addEventListener('load', function() {
+
 	var proxied = window.XMLHttpRequest.prototype.send;
 	var length;
+	var text;
+	var sourceTime;
+	var interval;
+
 	window.XMLHttpRequest.prototype.send = function () {
 		this.addEventListener('load', function () {
 			try {
 				var json = JSON.parse(this.responseText);
 				if ('text' in json) {
-					length = (json.text.length);
+					text = json.text;
+					length = json.text.length;
 					if (localStorage.stabiloCheat) {
 						document.getElementById('inputSpeed').value = +localStorage.stabiloCheat;
 						getTime(document.getElementById('inputSpeed').value);
+						rtBar.max = length;
+						unsafeWindow.txt = text;
 					}
+					setInt(inputSpeed.value, length);
 				}
 			} catch (e) {}
 		}.bind(this));
 		return proxied.apply(this, [].slice.call(arguments));
 	};
+
 
 	unsafeWindow.timeForSpeed = function(speed) {
 		var time = (60 / (speed / length));
@@ -34,14 +45,19 @@
 		return console.log(time);
 	};
 
-	var injPlace = document.getElementById('params').getElementsByClassName('rc')[0];
-	var injNode = document.createElement('div');
+
+
+
+// ******** TIME FOR SPEED INPUT ********
+
+	var tfsInputInjPlace = document.getElementById('params').getElementsByClassName('rc')[0];
+	var tfsInputInjNode = document.createElement('div');
 	var inputSpeed = document.createElement('input');
 	var tfs = document.createElement('div');
 
-	injNode.insert(inputSpeed);
-	injNode.insert(tfs);
-	injPlace.insertBefore(injNode, injPlace.childNodes[3]);
+	tfsInputInjNode.insert(inputSpeed);
+	tfsInputInjNode.insert(tfs);
+	tfsInputInjPlace.insertBefore(tfsInputInjNode, tfsInputInjPlace.childNodes[3]);
 
 	inputSpeed.style.setProperty('display', 'inline');
 	inputSpeed.style.setProperty('max-width', '40px');
@@ -51,6 +67,7 @@
 	inputSpeed.addEventListener('keyup', function () {
 		getTime(this.value);
 		localStorage.stabiloCheat = this.value;
+		setInt(inputSpeed.value, length);
 	});
 
 	tfs.style.setProperty('display', 'inline');
@@ -66,8 +83,75 @@
 		return;
 	}
 
+
+
+
+// ******** REMAINING TIME BAR ********
+
+	var rtBarInjPlace = document.getElementById('sortable');
+	var rtBarInjNode = document.createElement('div');
+	var rtBar = document.createElement('input');
+
+	rtBarInjNode.insert(rtBar);
+	console.log(rtBarInjPlace.childNodes);
+	rtBarInjPlace.insertBefore(rtBarInjNode, rtBarInjPlace.childNodes[1]);
+
+	rtBar.type = 'range';
+	rtBar.min = 0;
+	// rtBar.max at line 24
+	rtBar.step = 1;
+	rtBar.value = 0;
+	rtBar.setAttribute('disabled', 'true');
+	rtBar.style.setProperty('width', '100%');
+
+	rtBarInjNode.setAttribute('id', 'rtBar');
+
+	var b = [];
+	var timer;
+	var timeA = 0;
+	var passedTime = 0;
+
+	async function checkInputActivity() {
+		if (document.getElementById('inputtext').className == 'normal') {
+			timeA = Date.now();
+			timer = setTimeout(startBar(), interval);
+			return;
+		} else {
+			await sleep(10);
+			checkInputActivity();
+			return;
+		}
+	}
+
+	checkInputActivity();
+
+	function startBar() {
+		if (rtBar.value != rtBar.max) {
+			rtBar.value++;
+			passedTime += Date.now() - timeA;
+			b.push(Date.now() - timeA);
+			/*console.table('proshlo: ', Date.now() - timeA,
+						' over: ', Date.now() - timeA - interval,
+						' passedTime: ', passedTime,
+						' next after: ', (interval - (passedTime - (rtBar.value * interval))),
+						' rtBar: ', rtBar.value);*/
+			timer = setTimeout(startBar, (interval - (passedTime - (rtBar.value * interval))));
+			timeA = Date.now();
+			return;
+		}
+		clearInterval(timer);
+		var o = 0; for (let i = 0; i < b.length; i++) { o += b[i]; } console.log(o); // <-- complete in time
+		return;
+	}
+
+	function setInt(speed, length) {
+		interval = (1000 / (length / (60 / (speed / length)))).toFixed();
+		//console.log(interval * length);
+		return;
+	}
+
 	function sleep(ms) {
 		return new Promise(resolve => setTimeout(resolve, ms));
 	}
 
-})();
+}());
